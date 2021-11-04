e53795bb03a5915cf92644ca6807005fb6b3c6a2
Erik Lopes De Oliveira <erik.lopesdev@outlook.com>
2021-11-04 17:48:49 -0300

    consertando erro

--- src/App.js
+++ src/App.js
@@ -1,44 +1,52 @@
 import React, { Component } from 'react';
-import './App.css';
 import Home from './components/Home/Home';
+import './App.css';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      loading: true,
       pokemons: [],
+      pokeData: [],
+      loading: true,
     };
 
     this.doRequisition = this.doRequisition.bind(this);
     this.getPokemons = this.getPokemons.bind(this);
+    this.getPokeFromApi = this.getPokeFromApi.bind(this);
   }
 
   async componentDidMount() {
+    await this.doRequisition();
     await this.getPokemons();
   }
 
   async getPokemons() {
     const { pokemons } = this.state;
-    const pokemonsData = await this.doRequisition();
-    pokemonsData.results.forEach(async (poke) => {
-      const fetchGrab = await fetch(poke.url);
-      const fetchConversion = await fetchGrab.json();
-      pokemons.push(fetchConversion);
-    });
-    this.setState({ loading: false });
+    const pokeData = Promise.all(
+      pokemons.results.map(async (poke) => this.getPokeFromApi(poke.url)),
+    );
+
+    const pokeClear = await pokeData;
+    this.setState({ pokeData: pokeClear, loading: false });
+  }
+
+  async getPokeFromApi(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
   }
 
   async doRequisition() {
-    const api = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100%22');
-    const conversion = api.json();
-    return conversion;
+    const api = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
+    const conversion = await api.json();
+    this.setState({ pokemons: conversion });
   }
 
   render() {
-    const { pokemons, loading } = this.state;
-    return (
-      <Home loading={ loading } pokemons={ pokemons } />
+    const { pokemons, pokeData, loading } = this.state;
+    console.log(pokemons, pokeData);
+    return (<Home loading={ loading } pokemons={ pokeData } />
     );
   }
 }
